bfe96ddf16e031d6304dcc9eba906ad9d088c22b
Kais Ben Naceur <bennac_k@epita.fr>
2017-06-29 02:23:09 +0200

    [ELF] Fixed loading on segments

--- k/binary.c
+++ k/binary.c
@@ -20,6 +20,7 @@ int load_elf(multiboot_info_t *info)
   char *cmd = (char *)info->cmdline + 1;
   int fd = open(cmd, O_RDONLY);
   Elf32_Ehdr hdr;
+  u32 code = ((module_t*)info->mods_addr)[0].mod_end;
   if (fd != -1)
   {
     read(fd, &hdr, sizeof(Elf32_Ehdr));
@@ -38,21 +39,22 @@ int load_elf(multiboot_info_t *info)
       seek(fd, phdr.p_offset, SEEK_SET);
       if (PF_X & phdr.p_flags)
       {
-        read(fd, (void *)&gdt[1] + phdr.p_vaddr, phdr.p_filesz);
+        int r = read(fd, (void *)code + phdr.p_vaddr, phdr.p_filesz);
         if (phdr.p_filesz < phdr.p_memsz)
-          memset((void *)&gdt[1] + phdr.p_vaddr + phdr.p_filesz, 0,
+          memset((void *)code + phdr.p_vaddr + phdr.p_filesz, 0,
               phdr.p_memsz - phdr.p_filesz);
       }
       else
       {
-        read(fd, (void *)&gdt[2] + phdr.p_vaddr, phdr.p_filesz);
+        void *data = sbrk(phdr.p_memsz);
+        read(fd, data, phdr.p_filesz);
         if (phdr.p_filesz < phdr.p_memsz)
-          memset((void *)&gdt[2] + phdr.p_vaddr + phdr.p_filesz, 0,
+          memset(data + phdr.p_filesz, 0,
               phdr.p_memsz - phdr.p_filesz);
       }
     }
     close(fd);
-    return &gdt[1] + hdr.e_entry;
+    return code + hdr.e_entry;
   }
   return 0;
 }
